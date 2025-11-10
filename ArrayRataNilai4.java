c5f6bf465ead9b351f55703b5e06c4810737af7c
AthirahWahyu <athirahsasongko07@gmail.com>
2025-11-10 17:14:52 +0700

    Jobsheet 9 "Array 1" Percobaan 3

---
+++ ArrayRataNilai4.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ArrayRataNilai4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Membuat array nilaiMhs bertipe integer dengan kapasitas 10. 
+        // Kemudian mendeklarasi variable total dan rata2
+        int[] nilaiMhs = new int [10];
+        double total = 0;
+        double rata2;
+
+        // Mengisi array nilaiMhs dengan nilai dari input pengguna
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-"+ (i+1) +" : ");
+            nilaiMhs [i] = input.nextInt();
+        }
+        
+        // Menggunakan perulangan untuk menghitung jumlah 
+        // keseluruhan nilai dalam array nilaiMhs
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            total = total + nilaiMhs[i];
+        }
+
+        // Menghitung nilai rata-rata dengan cara nilai 
+        // total dibagi jumlah elemen dari array nilaiMhs
+        rata2 = total/nilaiMhs.length;
+        System.out.println("Rata - rata nilai = " + rata2);
+        input.close();
+    }
+    
+}
