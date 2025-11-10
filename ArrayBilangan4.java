1ae39609d945c9f8bfe978b8f74bdf7587938daa
AthirahWahyu <athirahsasongko07@gmail.com>
2025-11-10 09:41:35 +0700

    Jobsheet 9 "Array 1" Percobaan 1 modifikasi

--- ArrayBilangan4.java
+++ ArrayBilangan4.java
@@ -2,19 +2,12 @@ public class ArrayBilangan4 {
     public static void main(String[] args) {
 
         // Array bertipe integer dengan nama bil dengan kapasitas 4 elemen
-        int [] bil = new int[4];
+        int [] bil = {5, 13, -7, 17};
 
-        // Mengisi masing - masing elemen array bil 
-        bil [0] = 5;
-        bil [1] = 13;
-        bil [2] = -7;
-        bil [3] = 17;
-
-        // Output 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        // Output
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(bil[i]);
+        }
     }
 }
 
