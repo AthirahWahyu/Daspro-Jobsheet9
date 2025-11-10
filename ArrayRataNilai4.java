f098c87011b9e5c641057a77d263f7be5cf20614
AthirahWahyu <athirahsasongko07@gmail.com>
2025-11-11 00:26:12 +0700

    Percobaan 3 pertanyaan nomor 1

--- ArrayRataNilai4.java
+++ ArrayRataNilai4.java
@@ -1,32 +1,43 @@
-import java.util.Scanner;
+ import java.util.Scanner;
 
-public class ArrayRataNilai4 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+    public class ArrayRataNilai4 {
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
 
-        // Membuat array nilaiMhs bertipe integer dengan kapasitas 10. 
-        // Kemudian mendeklarasi variable total dan rata2
-        int[] nilaiMhs = new int [10];
-        double total = 0;
-        double rata2;
+            // Membuat array nilaiMhs bertipe integer dengan kapasitas 10. 
+            // Kemudian mendeklarasi variable total dan rata2
+            int[] nilaiMhs = new int [10];
+            double total = 0;
+            double rata2;
+            int jumlahLulus = 0; // untuk menghitung mahasiswa yang lulus
 
-        // Mengisi array nilaiMhs dengan nilai dari input pengguna
-        for (int i = 0; i < nilaiMhs.length; i++) {
-            System.out.print("Masukkan nilai mahasiswa ke-"+ (i+1) +" : ");
-            nilaiMhs [i] = input.nextInt();
-        }
+            // Mengisi array nilaiMhs dengan nilai dari input pengguna
+            for (int i = 0; i < nilaiMhs.length; i++) {
+                System.out.print("Masukkan nilai mahasiswa ke-"+ (i+1) +" : ");
+                    nilaiMhs [i] = input.nextInt();
+            }
         
-        // Menggunakan perulangan untuk menghitung jumlah 
-        // keseluruhan nilai dalam array nilaiMhs
-        for (int i = 0; i < nilaiMhs.length; i++) {
-            total = total + nilaiMhs[i];
-        }
+            // Menggunakan perulangan untuk menghitung jumlah 
+            // keseluruhan nilai dalam array nilaiMhs
+            for (int i = 0; i < nilaiMhs.length; i++) {
+                total = total + nilaiMhs[i];
 
-        // Menghitung nilai rata-rata dengan cara nilai 
-        // total dibagi jumlah elemen dari array nilaiMhs
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata - rata nilai = " + rata2);
-        input.close();
+            // Jika nilai mahasiswa lebih dari 70, menambahkan jumlahLulus
+                if (nilaiMhs[i] > 70) {
+                    jumlahLulus++;
+                }
+            }
+
+            // Menghitung nilai rata-rata dengan cara nilai 
+            // total dibagi jumlah elemen dari array nilaiMhs
+            rata2 = total/nilaiMhs.length;
+
+            // Output 
+            System.out.println("-------------------------------------");
+            System.out.println("Rata - rata nilai = " + rata2);
+            System.out.println("Jumlah mahasiswa yang lulus = " + jumlahLulus);
+
+            input.close();
+        }
     }
     
-}
