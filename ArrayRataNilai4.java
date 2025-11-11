a8b4f33b462a7c8afc331b2cad58f68ee1d73c64
AthirahWahyu <athirahsasongko07@gmail.com>
2025-11-11 14:19:38 +0700

    Jobsheet 9 "Array 1" Percobaan 3 modifikasi

--- ArrayRataNilai4.java
+++ ArrayRataNilai4.java
@@ -1,43 +1,55 @@
- import java.util.Scanner;
+import java.util.Scanner;
 
-    public class ArrayRataNilai4 {
-        public static void main(String[] args) {
-            Scanner input = new Scanner(System.in);
+public class ArrayRataNilai4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-            // Membuat array nilaiMhs bertipe integer dengan kapasitas 10. 
-            // Kemudian mendeklarasi variable total dan rata2
-            int[] nilaiMhs = new int [10];
-            double total = 0;
-            double rata2;
-            int jumlahLulus = 0; // untuk menghitung mahasiswa yang lulus
+        // Input jumlah mahasiswa 
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int jumlahMhs = input.nextInt();
 
-            // Mengisi array nilaiMhs dengan nilai dari input pengguna
-            for (int i = 0; i < nilaiMhs.length; i++) {
-                System.out.print("Masukkan nilai mahasiswa ke-"+ (i+1) +" : ");
-                    nilaiMhs [i] = input.nextInt();
-            }
-        
-            // Menggunakan perulangan untuk menghitung jumlah 
-            // keseluruhan nilai dalam array nilaiMhs
-            for (int i = 0; i < nilaiMhs.length; i++) {
-                total = total + nilaiMhs[i];
+        // Membuat array dengan ukuran sesuai input pengguna 
+        int[] nilaiMhs  = new int[jumlahMhs];
 
-            // Jika nilai mahasiswa lebih dari 70, menambahkan jumlahLulus
-                if (nilaiMhs[i] > 70) {
-                    jumlahLulus++;
-                }
-            }
+        // Membuat array nilaiMhs bertipe integer dengan kapasitas 10. 
+        // Kemudian mendeklarasi variable total dan rata2
+        double totalLulus = 0;
+        double totalTidakLulus = 0;
+        int jumlahLulus = 0; // untuk menghitung mahasiswa yang lulus
+        int jumlahTidakLulus = 0;
 
-            // Menghitung nilai rata-rata dengan cara nilai 
-            // total dibagi jumlah elemen dari array nilaiMhs
-            rata2 = total/nilaiMhs.length;
-
-            // Output 
-            System.out.println("-------------------------------------");
-            System.out.println("Rata - rata nilai = " + rata2);
-            System.out.println("Jumlah mahasiswa yang lulus = " + jumlahLulus);
-
-            input.close();
+        // Mengisi array nilaiMhs dengan nilai dari input pengguna
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-"+ (i+1) +" : ");
+            nilaiMhs [i] = input.nextInt();
         }
+        
+        // Menghitung total nilai lulus dan tidak lulus
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            if (nilaiMhs[i] > 70) {
+                totalLulus = totalLulus + nilaiMhs[i];
+                jumlahLulus++;
+        } else {
+                totalTidakLulus = totalTidakLulus + nilaiMhs[i];
+                jumlahTidakLulus++;
+        } 
     }
-    
+
+        // Menghitug rata - rata 
+        double rata2Lulus = 0, rata2TidakLulus = 0;
+
+        if (jumlahLulus > 0) {
+            rata2Lulus = totalLulus / jumlahLulus;
+        }
+        if (jumlahTidakLulus > 0) {
+            rata2TidakLulus = totalTidakLulus / jumlahTidakLulus;
+        }
+
+        // Output 
+        System.out.println("-------------------------------------");
+        System.out.println("Rata - rata nilai lulus = " + rata2Lulus);
+        System.out.println("Rata - rata nilai tidak lulus = " + rata2TidakLulus);
+
+        input.close();
+    }
+}
